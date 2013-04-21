28927f142331a9f40b72ea9ea2acc7f91c6c0d83
Akhilesh Nayak <nayakhilesh@gmail.com>
2013-04-20 22:25:56 -0400

    refactoring + cyk algo debugging (still needs some work)

--- pcfg.rb
+++ pcfg.rb
@@ -8,13 +8,7 @@ require 'optparse'
 
 def write_replaced_file(filename)
 
-  freq_hash = {}
-  File.open(filename, 'r').each_line do |line|
-    #puts line
-    tree = JSON.parse(line)
-    add_freq(tree,freq_hash)
-  end
-  
+  freq_hash = get_freq_hash(filename)
   #p freq_hash
   
   infrequent_words = freq_hash.reject { |word, freq| freq >= 5 }
@@ -30,6 +24,16 @@ def write_replaced_file(filename)
     end
   end
 
+end
+
+def get_freq_hash(filename)
+  freq_hash = {}
+  File.open(filename, 'r').each_line do |line|
+    #puts line
+    tree = JSON.parse(line)
+    add_freq(tree,freq_hash)
+  end
+  return freq_hash
 end
 
 def add_freq(tree,freq_hash)
@@ -62,19 +66,26 @@ end
 
 class MaxLikelihoodEstimator
 
-  attr_accessor :nonterminal_count, :binary_rules 
-  
-  def initialize(counts_file)
+  attr_accessor :nonterminal_count, :binary_rules, :unary_rules 
+                                                    # TODO remove
+  def initialize(counts_file,frequent_words)
     @nonterminal_count = {}
     @unaryrule_count = {}
     @binaryrule_count = {}
     @binary_rules = {}
+    @unary_rules = {}
+    @frequent_words = frequent_words
     File.open(counts_file, 'r').each_line do |line|
       arr = line.split
       if arr[1] == 'NONTERMINAL'
         @nonterminal_count[arr[2].freeze] = arr[0].to_i
       elsif arr[1] == 'UNARYRULE'
         @unaryrule_count[[arr[2],arr[3]].freeze] = arr[0].to_i
+        if @unary_rules.has_key?(arr[2])
+          @unary_rules[arr[2].freeze] << arr[3]
+        else
+          @unary_rules[arr[2].freeze] = [arr[3]]
+        end
       elsif arr[1] == 'BINARYRULE'
         @binaryrule_count[[arr[2],arr[3],arr[4]].freeze] = arr[0].to_i
         if @binary_rules.has_key?(arr[2])
@@ -95,13 +106,19 @@ class MaxLikelihoodEstimator
   
   def estimate(x,y,z=nil)
     if z == nil
-      if @unaryrule_count.has_key?([x,y])
-        @unaryrule_count[[x,y]]/@nonterminal_count[x].to_f
+      # unary rule
+      if @frequent_words.has_key?(y)
+        if @unaryrule_count.has_key?([x,y])
+          @unaryrule_count[[x,y]]/@nonterminal_count[x].to_f
+        else
+         nil
+        end
       elsif @unaryrule_count.has_key?([x,'_RARE_'])
         @unaryrule_count[[x,'_RARE_']]/@nonterminal_count[x].to_f
       else 
         nil
       end
+    # binary rule 
     elsif @binaryrule_count.has_key?([x,y,z])
       @binaryrule_count[[x,y,z]]/@nonterminal_count[x].to_f
     else
@@ -114,65 +131,107 @@ end
 def cky_algorithm(sentence,estimator)
 
   #p sentence
-  cap_n = estimator.nonterminal_count.keys
+  #cap_n = estimator.nonterminal_count.keys
   n = sentence.length
   # initialization
   pi = {}
   (1..n).each do |i|
-    cap_n.each do |cap_x|
+    estimator.unary_rules.keys.each do |cap_x|
       q = estimator.estimate(cap_x,sentence[i-1])
-      pi[[i,i,cap_x].freeze] = (q == nil) ? 0.0 : q
+      if q != nil
+        puts sentence[i-1] + ' ' + cap_x + ' ' + q.to_s
+        pi[[i,i,cap_x].freeze] = q
+      end
     end
   end
   #p pi
+  #exit
   
-  cap_r = estimator.binary_rules
+  #cap_r = estimator.binary_rules
+  bp = {}
   # algorithm
   (1..(n-1)).each do |l|
+    puts 'length=' + l.to_s
     (1..(n-l)).each do |i|
       j = i+l
-      cap_n.each do |cap_x|
+      puts 'from %d to %d' %[i,j]
+      max = -1.0
+      (i..(j-1)).each do |s|
+        puts 'for split (%d,%d),(%d,%d)' %[i,s,s+1,j]
+        estimator.binary_rules.keys.each do |cap_x|
       
         # calculating max here
-        if cap_r[cap_x] != nil
-          max = 0.0
-          cap_r[cap_x].each do |cap_yz|
+        
+          estimator.binary_rules[cap_x].each do |cap_yz|
             cap_y = cap_yz[0]
             cap_z = cap_yz[1]
-            (i..(j-1)).each do |s|
-              puts 'new'
-              p estimator.estimate(cap_x,cap_y,cap_z)
-              p pi[[i,s,cap_y]]
-              p pi[[s+1,j,cap_z]]
-              val = estimator.estimate(cap_x,cap_y,cap_z) * pi[[i,s,cap_y]] * pi[[s+1,j,cap_z]]
+            
+              
+            if pi.has_key?([i,s,cap_y]) and pi.has_key?([s+1,j,cap_z])
+              val = estimator.estimate(cap_x,cap_y,cap_z) *
+                    pi[[i,s,cap_y]] *
+                    pi[[s+1,j,cap_z]]
+              puts 'prob=%.15f, q(%s,%s,%s)=%.15f, pi(%d,%d,%s)=%.15f, pi(%d,%d,%s)=%.15f' %[val,cap_x,cap_y,
+              cap_z,estimator.estimate(cap_x,cap_y,cap_z),i,s,cap_y,pi[[i,s,cap_y]],s+1,j,cap_z,pi[[s+1,j,cap_z]]]
               if val > max
-                pi[[i,j,cap_x]] = val
+                pi[[i,j,cap_x].freeze] = val
+                bp[[i,j,cap_x].freeze] = [cap_y,cap_z,s]
                 max = val
               end
             end
           end
         end
-        
+
+        if max < -0.99 and max > -1.01
+            puts 'No valid rules found'
+        end
+
       end
+         
     end
   end
-  p pi
+  #p pi
+  #p bp
+  
+  p create_tree(1,n,'SBARQ',bp,sentence)
   
 end
 
-def main
+def create_tree(start,_end,tag,bp,sentence)
+  if start == _end
+    return [tag,sentence[start-1]]
+  end
+  arr = bp[[start,_end,tag]]
+  y = arr[0]
+  z = arr[1]
+  s = arr[2]
+  tree = [tag,create_tree(start,s,y,bp,sentence),create_tree(s+1,_end,z,bp,sentence)]
+  return tree
+end
+
+def usage
+  'Usage: pcfg.rb [--writereplaced --trainingfile FILE] | [--trainingfile FILE --countsfile FILE --inputfile FILE]'
+end
+
+def parse_options
+
   options = {}
   optparse = OptionParser.new do |opts|
   
-    opts.banner = 'Usage: pcfg.rb --replacefile FILE --countsfile FILE --inputfile FILE'
+    opts.banner = usage
     
-    options[:replace_file] = nil
-    opts.on('-r', '--replacefile FILE', 'File with training data to replace infrequent words with _RARE_') do |filename|
-      options[:replace_file] = filename
+    options[:write_replaced] = false
+    opts.on('-w', '--writereplaced', 'Write file \'($trainingfile).replaced\' with infrequent words in $trainingfile replaced with _RARE_') do
+     options[:write_replaced] = true
+    end
+    
+    options[:training_file] = nil
+    opts.on('-t', '--trainingfile FILE', 'File with training data') do |filename|
+      options[:training_file] = filename
     end
     
     options[:counts_file] = nil
-    opts.on('-c', '--countsfile FILE', 'File with rule counts') do |filename|
+    opts.on('-c', '--countsfile FILE', 'File with rule counts derived from \'($trainingfile).replaced\'') do |filename|
       options[:counts_file] = filename
     end
     
@@ -189,22 +248,45 @@ def main
   end
   
   optparse.parse!
+  return options
+  
+end
+
+def main
+  
+  options = parse_options
   #p options
 
-  unless options[:replace_file] == nil
-    write_replaced_file(options[:replace_file])    
-  end
-
-  unless options[:counts_file] == nil
-    estimator = MaxLikelihoodEstimator.new(options[:counts_file])
+  if options[:write_replaced]
+    if options[:training_file]
+      write_replaced_file(options[:training_file])
+    else
+     puts usage
+     exit
+    end
+  else
+    if options[:training_file] and options[:counts_file] and options[:input_file]
+      
+      freq_hash = get_freq_hash(options[:training_file])
+      frequent_words = freq_hash.reject { |word, freq| freq < 5 }  
+      
+      estimator = MaxLikelihoodEstimator.new(options[:counts_file],frequent_words)
+    
+      #p estimator.estimate('VP','PP','NP')
+      #p estimator.estimate('ADVP+ADV','blech')
+      #p estimator.estimate('ADVP+ADV','gagagoogoo')
+    
+      # TODO - is freeze needed?
+    
+      sentence = 'What are geckos ?'
+      cky_algorithm(sentence.split,estimator) 
+  
+    else
+      puts usage
+      exit
+    end
   end
   
-  p estimator.estimate('VP','PP','NP')
-  p estimator.estimate('ADVP+ADV','blech')
-  p estimator.estimate('ADVP+ADV','gagagoogoo')
-  
-  cky_algorithm(%w(hello there again),estimator)
-
 end
 
 if __FILE__ == $0
